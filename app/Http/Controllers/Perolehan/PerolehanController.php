27f53fe56bed8aab4491a8daf4f3a8585a27babf
RahmaanM17 <ardrah17@gmail.com>
2024-02-21 17:10:56 +0700

    feat: perolehan done

--- app/Http/Controllers/Perolehan/PerolehanController.php
+++ app/Http/Controllers/Perolehan/PerolehanController.php
@@ -20,6 +20,7 @@ class PerolehanController extends Controller
         $kibs = $request->only('detail');
         $bap['kodejenistransaksi'] = 101;
         $bap['tanggalbap'] = convertAlphabeticalToNumberDate($request->tanggalbap);
+
         $bap['kodeurusan'] = $must['kodeurusan'] = 1;
         $bap['kodesuburusan'] = $must['kodesuburusan'] = 0;
         $bap['kodeorganisasi'] = $must['kodeorganisasi'] = 0;
@@ -30,28 +31,65 @@ class PerolehanController extends Controller
         $bap['tahunorganisasi'] = $must['tahunorganisasi'] = 2024;
         $kodebap = DB::table('bap')->insertGetId($bap, 'kodebap');
         foreach ($kibs['detail'] as $index => $kib) {
-            $kib = array_merge($kib, $must);
-            $kib['nilaibarang'] = convertStringToNumber($kib['nilaibarang']);
-            $kib['uraibarang'] = $kib['urai'];
-            $kib['tahunperolehan'] = 2024;
-            $kib['kodepemilik'] = 34;
-            $kib['kodeklasifikasi'] = ($kib['nilaibarang'] >= intval(classificationType($kib)->nilai)) ? 1 : 2;
-            $kib['kodeklasifikasi_u'] = ($kib['nilaibarang'] >= intval(classificationType($kib)->nilai)) ? 1 : 2;
-            $kib['koderegister'] = getKoderegister($kib);
             $jumlah = $kib['jumlah'];
-            unset($kib['select-asal-usul-barang-perolehan-aset'], $kib['jumlah'], $kib['iddetail'], $kib['kodemasterbarang'], $kib['urai']);
             for ($i = 0; $i < (int)$jumlah; $i++) {
-                $kodekib = DB::table('kib')->insertGetId($kib, 'kodekib');
-                $kib['nilaitransaksi'] = $kib['nilaibarang'];
-                $kib['kodekib'] = $kodekib;
-                unset($kib['deskripsibarang'], $kib['nilaibarang'], $kib['luas'], $kib['tahunperolehan'], $kib['kodeasalusul'], $kib['qrcode_foto'], $kib['kodepemilik']);
-                $kib['kodebap'] = $kodebap;
-                $kib['uraiorganisasi'] = 'URUSAN PEMERINTAHAN WAJIB YANG BERKAITAN DENGAN PELAYANAN DASAR';
-                $kib['kodejenistransaksi'] = 101;
-                $kib['kodejurnal'] = 0;
-                $kib['tanggaltransaksi'] = convertAlphabeticalToNumberDate($request->tanggalbap);
-                $kib['tanggalpenyusutan'] = convertAlphabeticalToNumberDate($request->tanggalbap);
-                DB::table('kibtransaksi')->insert($kib);
+                $copied = array_merge($kib, $must);
+                $copied['nilaibarang'] = convertStringToNumber($kib['nilaibarang']);
+                if (isset($kib['tglimb'])) {
+                    $copied['tglimb'] = convertAlphabeticalToNumberDate($kib['tglimb']);
+                }
+                $copied['uraibarang'] = $kib['urai'];
+                $copied['tahunperolehan'] = 2024;
+                $copied['kodepemilik'] = 34;
+                $copied['uraiorganisasi'] = 'URUSAN PEMERINTAHAN WAJIB YANG BERKAITAN DENGAN PELAYANAN DASAR';
+                $copied['kodeklasifikasi'] = ($copied['nilaibarang'] >= intval(classificationType($copied)->nilai)) ? 1 : 2;
+                $copied['kodeklasifikasi_u'] = ($copied['nilaibarang'] >= intval(classificationType($copied)->nilai)) ? 1 : 2;
+                $copied['koderegister'] = getKoderegister($copied);
+                unset($copied['select-asal-usul-barang-perolehan-aset'], $copied['jumlah'], $copied['iddetail'], $copied['kodemasterbarang'], $copied['urai']);
+                $kodekib = DB::table('kib')->insertGetId($copied, 'kodekib');
+                unset(
+                    $copied['deskripsibarang'],
+                    $copied['nilaibarang'],
+                    $copied['luas'],
+                    $copied['tahunperolehan'],
+                    $copied['kodeasalusul'],
+                    $copied['qrcode_foto'],
+                    $copied['kodepemilik'],
+                    $copied['kodekondisi'],
+                    $copied['tahunpembuatan'],
+                    $copied['kodegolonganbarang'],
+                    $copied['kodewarna'],
+                    $copied['bahan'],
+                    $copied['nopabrik'],
+                    $copied['norangka'],
+                    $copied['nomesin'],
+                    $copied['nobpkb'],
+                    $copied['kodesatuan'],
+                    $copied['merktype'],
+                    $copied['ukuran'],
+                    $copied['alamat'],
+                    $copied['noimb'],
+                    $copied['tglimb'],
+                    $copied['kodestatustanah'],
+                    $copied['luaslantai'],
+                    $copied['kodehak'],
+                    $copied['konstruksi'],
+                    $copied['panjang'],
+                    $copied['lebar'],
+                    $copied['nilaihibahtotal'],
+                    $copied['judul'],
+                    $copied['pencipta'],
+                    $copied['asaldaerah'],
+                    $copied['jenis'],
+                );
+                $copied['nilaitransaksi'] = $kib['nilaibarang'];
+                $copied['kodekib'] = $kodekib;
+                $copied['kodebap'] = $kodebap;
+                $copied['kodejenistransaksi'] = 101;
+                $copied['kodejurnal'] = 0;
+                $copied['tanggaltransaksi'] = convertAlphabeticalToNumberDate($request->tanggalbap);
+                $copied['tanggalpenyusutan'] = convertAlphabeticalToNumberDate($request->tanggalbap);
+                DB::table('kibtransaksi')->insert($copied);
             }
         }
         return response()->json(['message' => 'berhasil menambahkan perolehan']);
