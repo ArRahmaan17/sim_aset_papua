e7c3e4bf8a97734e9305b05a0f77bdef506db85d
RahmaanM17 <ardrah17@gmail.com>
2024-03-07 16:58:35 +0700

    feat: menu master barang create,read,update done

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -20,7 +20,7 @@ class BarangController extends Controller
         $data_barang = DB::table('masterbarang')
             ->select(
                 DB::raw(
-                    "CONCAT(kodegolongan, '', kodebidang, '', kodekelompok, '', kodesub, '', kodesubsub) as id"
+                    "CONCAT(kodegolongan, '.', kodebidang, '.', kodekelompok, '.', kodesub, '.', kodesubsub) as id"
                 ),
                 DB::raw("CONCAT(kodegolongan, '.', kodebidang, '.', kodekelompok, '.', kodesub, '.', kodesubsub, ' ', urai) as text"),
                 DB::raw(
@@ -30,29 +30,30 @@ class BarangController extends Controller
                                                 and kodekelompok = 0
                                                 and kodesub = 0
                                                 and kodesubsub = 0 
-                                        then CONCAT(kodegolongan, '0', '0', '0', '0')
+                                        then CONCAT(kodegolongan, '.0', '.0', '.0', '.0')
                                             when kodegolongan <> 0
                                                 and kodebidang <> 0
                                                 and kodekelompok <> 0
                                                 and kodesub = 0
                                                 and kodesubsub = 0 
-                                        then CONCAT(kodegolongan, kodebidang, '0', '0', '0')
+                                        then CONCAT(kodegolongan,'.', kodebidang, '.0', '.0', '.0')
                                             when kodegolongan <> 0
                                                 and kodebidang <> 0
                                                 and kodekelompok <> 0
                                                 and kodesub <> 0
                                                 and kodesubsub = 0 
-                                        then CONCAT(kodegolongan, kodebidang, kodekelompok, '0', '0')
+                                        then CONCAT(kodegolongan, '.', kodebidang,'.', kodekelompok, '.0', '.0')
                                             when kodegolongan <> 0
                                                 and kodebidang <> 0
                                                 and kodekelompok <> 0
                                                 and kodesub <> 0
                                                 and kodesubsub <> 0 
-                                        then CONCAT(kodegolongan, kodebidang, kodekelompok, kodesub, '0')
+                                        then CONCAT(kodegolongan,  '.', kodebidang, '.',  kodekelompok, '.',  kodesub, '.0')
                                     else '0'
                                     end) as parent"
                 )
             )
+            ->where('kodegolongan', 131)
             ->orderByRaw(" kodegolongan, kodebidang, kodekelompok, kodesub, kodesubsub")
             ->get()
             ->toArray();
@@ -67,20 +68,28 @@ class BarangController extends Controller
         return response()->json($message, $status);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        DB::beginTransaction();
+        try {
+            $kodebarang = explode('.', $request->kodebarang);
+            DB::table('masterbarang')->insert([
+                'kodegolongan' => $kodebarang[0],
+                'kodebidang' => $kodebarang[1],
+                'kodekelompok' => $kodebarang[2],
+                'kodesub' => $kodebarang[3],
+                'kodesubsub' => $kodebarang[4],
+                'urai' => $request->urai
+            ]);
+            return response()->json(['message' => 'Input master barang berhasil'], 200);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return response()->json(['message' => 'Input master barang gagal'], 422);
+        }
     }
 
     /**
@@ -88,7 +97,26 @@ class BarangController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $kodebarang = explode('.', $id);
+
+        $data = DB::table('masterbarang')->select(
+            DB::raw("CONCAT(kodegolongan, '.', kodebidang, '.', kodekelompok, '.', kodesub, '.', kodesubsub) as kodebarang"),
+            'urai'
+        )->where([
+            'kodegolongan' => $kodebarang[0],
+            'kodebidang' => $kodebarang[1],
+            'kodekelompok' => $kodebarang[2],
+            'kodesub' => $kodebarang[3],
+            'kodesubsub' => $kodebarang[4],
+        ])->first();
+        if ($data) {
+            $message = ['message' => 'detail data master barang ditemukan', 'data' => $data];
+            $status = 200;
+        } else {
+            $status = 404;
+            $message = ['message' => 'detail data master barang tidak ditemukan', 'data' => $data];
+        }
+        return response()->json($message, $status);
     }
 
     /**
@@ -104,7 +132,23 @@ class BarangController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        DB::beginTransaction();
+        try {
+            $kodebarang = explode('.', $id);
+            DB::table('masterbarang')->where([
+                'kodegolongan' => $kodebarang[0],
+                'kodebidang' => $kodebarang[1],
+                'kodekelompok' => $kodebarang[2],
+                'kodesub' => $kodebarang[3],
+                'kodesubsub' => $kodebarang[4]
+            ])->update([
+                'urai' => $request->urai
+            ]);
+            return response()->json(['message' => 'Ubah master barang berhasil'], 200);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return response()->json(['message' => 'Ubah master barang gagal'], 422);
+        }
     }
 
     /**
