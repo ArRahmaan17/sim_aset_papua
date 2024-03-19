3b184f8dfc774047ca1135168dba78196e565c9a
RahmaanM17 <ardrah17@gmail.com>
2024-03-19 11:46:18 +0700

    feat: master status tanah

--- app/Http/Controllers/StatusTanahController.php
+++ app/Http/Controllers/StatusTanahController.php
@@ -6,24 +6,27 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class WarnaController extends Controller
+class StatusTanahController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        return view('layout.warna.index');
+        return view('layout.statustanah.index');
     }
 
+    /**
+     * Show the form for creating a new resource.
+     */
     public function dataTable(Request $request)
     {
-        $totalData = DB::table('masterwarna')
-            ->orderBy('kodemasamanfaat', 'asc')
+        $totalData = DB::table('masterstatustanah')
+            ->orderBy('kodestatustanah', 'asc')
             ->count();
         $totalFiltered = $totalData;
         if (empty($request['search']['value'])) {
-            $assets = DB::table('masterwarna')
+            $assets = DB::table('masterstatustanah')
                 ->select('*');
 
             if ($request['length'] != '-1') {
@@ -31,14 +34,14 @@ class WarnaController extends Controller
                     ->offset($request['start']);
             }
             if (isset($request['order'][0]['column'])) {
-                $assets->orderByRaw('warna ' . $request['order'][0]['dir']);
+                $assets->orderByRaw('statustanah ' . $request['order'][0]['dir']);
             }
             $assets = $assets->get();
         } else {
-            $assets = DB::table('masterwarna')->select('*')
-                ->where('warna', 'like', '%' . $request['search']['value'] . '%');
+            $assets = DB::table('masterstatustanah')->select('*')
+                ->where('statustanah', 'like', '%' . $request['search']['value'] . '%');
             if (isset($request['order'][0]['column'])) {
-                $assets->orderByRaw('warna ' . $request['order'][0]['dir']);
+                $assets->orderByRaw('statustanah ' . $request['order'][0]['dir']);
             }
             if ($request['length'] != '-1') {
                 $assets->limit($request['length'])
@@ -46,10 +49,10 @@ class WarnaController extends Controller
             }
             $assets = $assets->get();
 
-            $totalFiltered = DB::table('masterwarna')->select('*')
-                ->where('warna', 'like', '%' . $request['search']['value'] . '%');
+            $totalFiltered = DB::table('masterstatustanah')->select('*')
+                ->where('statustanah', 'like', '%' . $request['search']['value'] . '%');
             if (isset($request['order'][0]['column'])) {
-                $totalFiltered->orderByRaw('warna ' . $request['order'][0]['dir']);
+                $totalFiltered->orderByRaw('statustanah ' . $request['order'][0]['dir']);
             }
             $totalFiltered = $totalFiltered->count();
         }
@@ -57,9 +60,9 @@ class WarnaController extends Controller
         foreach ($assets as $index => $item) {
             $row = [];
             $row[] = $index + 1;
-            $row[] = $item->warna;
+            $row[] = $item->statustanah;
             $row[] = "<button class='btn btn-warning edit' ><i class='bx bxs-pencil'></i> Edit</button><button class='btn btn-danger delete'><i class='bx bxs-trash-alt' ></i> Hapus</button>";
-            $row[] = $item->kodewarna;
+            $row[] = $item->kodestatustanah;
             $dataFiltered[] = $row;
         }
         $response = [
@@ -71,6 +74,7 @@ class WarnaController extends Controller
 
         return Response()->json($response, 200);
     }
+
     /**
      * Store a newly created resource in storage.
      */
@@ -79,19 +83,18 @@ class WarnaController extends Controller
         $data = $request->except('_token');
         DB::beginTransaction();
         try {
-            $unique = DB::table('masterwarna')
-                ->where('warna', 'like', "'%" . $request->warna . "%'")
+            $unique = DB::table('masterstatustanah')
+                ->where('statustanah', 'like', "'%" . $request->statustanah . "%'")
                 ->count();
             if ($unique != 0) {
-                throw new Exception('gagal melakukan simpan data warna, terdapat duplikasi data warna', 422);
+                throw new Exception('gagal melakukan simpan data status tanah, terdapat duplikasi data status tanah', 422);
             }
-            DB::table('masterwarna')->insert($data);
+            DB::table('masterstatustanah')->insert($data);
             DB::commit();
-            $data = DB::table('masterwarna')->get()->toArray();
             $status = 200;
-            $message = ['message' => 'Master warna berhasil ditambahkan', 'data' => $data];
+            $message = ['message' => 'Master status tanah berhasil ditambahkan'];
         } catch (Exception $th) {
-            $message = ['message' => 'Master warna gagal ditambahkan'];
+            $message = ['message' => 'Master status tanah gagal ditambahkan'];
             if ($th->getCode() == 422) {
                 $message = ['message' => $th->getMessage()];
             }
@@ -106,13 +109,15 @@ class WarnaController extends Controller
      */
     public function show(string $id)
     {
-        $data = DB::table('masterwarna')->where('kodewarna', $id)->first();
+        $data = DB::table('masterstatustanah')
+            ->where('kodestatustanah', $id)
+            ->first();
         if ($data) {
             $status = 200;
-            $message = ['message' => "data master warna berhasil di temukan", 'data' => $data];
+            $message = ['message' => "data master status tanah berhasil di temukan", 'data' => $data];
         } else {
             $status = 404;
-            $message = ['message' => "data master warna gagal di temukan", 'data' => $data];
+            $message = ['message' => "data master status tanah gagal di temukan", 'data' => $data];
         }
         return response()->json($message, $status);
     }
@@ -125,18 +130,20 @@ class WarnaController extends Controller
         $data = $request->except('_token');
         DB::beginTransaction();
         try {
-            $unique = DB::table('masterwarna')
-                ->where('warna', 'like', "'%" . $request->warna . "%'")
+            $unique = DB::table('masterstatustanah')
+                ->where('statustanah',   $request->statustanah)
                 ->count();
             if ($unique != 0) {
-                throw new Exception('gagal melakukan simpan data warna, terdapat duplikasi data warna', 422);
+                throw new Exception('gagal melakukan simpan data status tanah, terdapat duplikasi data status tanah', 422);
             }
-            DB::table('masterwarna')->where('kodewarna', $id)->update($data);
+            DB::table('masterstatustanah')
+                ->where('kodestatustanah', $id)
+                ->update($data);
             DB::commit();
             $status = 200;
-            $message = ['message' => 'Master warna berhasil diubah'];
+            $message = ['message' => 'Master status tanah berhasil diubah'];
         } catch (Exception $th) {
-            $message = ['message' => 'Master warna gagal diubah'];
+            $message = ['message' => 'Master status tanah gagal diubah'];
             if ($th->getCode() == 422) {
                 $message = ['message' => $th->getMessage()];
             }
@@ -153,17 +160,17 @@ class WarnaController extends Controller
     {
         DB::beginTransaction();
         try {
-            $count = DB::table('kib')->where('kodewarna', $id)->count();
+            $count = DB::table('kib')->where('kodestatustanah', $id)->count();
             if ($count > 0) {
                 throw new Exception('data sudah di gunakan di tabel lain, mohon hapus terlebih dahulu data tersebut', 422);
             }
-            DB::table('masterwarna')->where('kodewarna', $id)->delete();
+            DB::table('mastersatuan')->where('kodestatustanah', $id)->delete();
             DB::commit();
-            $message = ['message' => 'berhasil menghapus data master warna'];
+            $message = ['message' => 'berhasil menghapus data master status tanah'];
             $status = 200;
         } catch (Exception $th) {
             DB::rollBack();
-            $message = ['message' => 'gagal menghapus data master warna'];
+            $message = ['message' => 'gagal menghapus data master status tanah'];
             if ($th->getCode() == 422) {
                 $message = ['message' => $th->getMessage()];
             }
