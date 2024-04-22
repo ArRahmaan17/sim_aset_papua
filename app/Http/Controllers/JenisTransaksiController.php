5844b5fa2566d74dc0b84b07d16d4bbc2142228f
RahmaanM17 <ardrah17@gmail.com>
2024-04-22 11:03:15 +0700

    update(style): laravel pint formatter

--- app/Http/Controllers/JenisTransaksiController.php
+++ app/Http/Controllers/JenisTransaksiController.php
@@ -29,14 +29,14 @@ class JenisTransaksiController extends Controller
                     ->offset($request['start']);
             }
             if (isset($request['order'][0]['column'])) {
-                $assets->orderByRaw('jenistransaksi ' . $request['order'][0]['dir']);
+                $assets->orderByRaw('jenistransaksi '.$request['order'][0]['dir']);
             }
             $assets = $assets->get();
         } else {
             $assets = DB::table('masterjenistransaksi')->select('*')
-                ->where('jenistransaksi', 'like', '%' . $request['search']['value'] . '%');
+                ->where('jenistransaksi', 'like', '%'.$request['search']['value'].'%');
             if (isset($request['order'][0]['column'])) {
-                $assets->orderByRaw('jenistransaksi ' . $request['order'][0]['dir']);
+                $assets->orderByRaw('jenistransaksi '.$request['order'][0]['dir']);
             }
             if ($request['length'] != '-1') {
                 $assets->limit($request['length'])
@@ -46,9 +46,9 @@ class JenisTransaksiController extends Controller
 
             $totalFiltered = DB::table('masterjenistransaksi')
                 ->select('*')
-                ->where('jenistransaksi', 'like', '%' . $request['search']['value'] . '%');
+                ->where('jenistransaksi', 'like', '%'.$request['search']['value'].'%');
             if (isset($request['order'][0]['column'])) {
-                $totalFiltered->orderByRaw('jenistransaksi ' . $request['order'][0]['dir']);
+                $totalFiltered->orderByRaw('jenistransaksi '.$request['order'][0]['dir']);
             }
             $totalFiltered = $totalFiltered->count();
         }
@@ -56,8 +56,8 @@ class JenisTransaksiController extends Controller
         foreach ($assets as $index => $item) {
             $row = [];
             $row[] = $request['start'] + ($index + 1);
-            $row[] = '' . $item->jenistransaksi . '(' . $item->tipetransaksi . ')';
-            $row[] = '' . $item->keterangan;
+            $row[] = ''.$item->jenistransaksi.'('.$item->tipetransaksi.')';
+            $row[] = ''.$item->keterangan;
             $row[] = "<button class='btn btn-warning edit' ><i class='bx bxs-pencil'></i> Edit</button><button class='btn btn-danger delete'><i class='bx bxs-trash-alt' ></i> Hapus</button>";
             $row[] = $item->kodejenistransaksi;
             $dataFiltered[] = $row;
--- app/Http/Controllers/RuanganController.php
+++ app/Http/Controllers/RuanganController.php
@@ -29,14 +29,14 @@ class RuanganController extends Controller
                     ->offset($request['start']);
             }
             if (isset($request['order'][0]['column'])) {
-                $assets->orderByRaw('ruang ' . $request['order'][0]['dir']);
+                $assets->orderByRaw('ruang '.$request['order'][0]['dir']);
             }
             $assets = $assets->get();
         } else {
             $assets = DB::table('masterruang')->select('*')
-                ->where('ruang', 'like', '%' . $request['search']['value'] . '%');
+                ->where('ruang', 'like', '%'.$request['search']['value'].'%');
             if (isset($request['order'][0]['column'])) {
-                $assets->orderByRaw('ruang ' . $request['order'][0]['dir']);
+                $assets->orderByRaw('ruang '.$request['order'][0]['dir']);
             }
             if ($request['length'] != '-1') {
                 $assets->limit($request['length'])
@@ -46,9 +46,9 @@ class RuanganController extends Controller
 
             $totalFiltered = DB::table('masterruang')
                 ->select('*')
-                ->where('ruang', 'like', '%' . $request['search']['value'] . '%');
+                ->where('ruang', 'like', '%'.$request['search']['value'].'%');
             if (isset($request['order'][0]['column'])) {
-                $totalFiltered->orderByRaw('ruang ' . $request['order'][0]['dir']);
+                $totalFiltered->orderByRaw('ruang '.$request['order'][0]['dir']);
             }
             $totalFiltered = $totalFiltered->count();
         }
@@ -56,7 +56,7 @@ class RuanganController extends Controller
         foreach ($assets as $index => $item) {
             $row = [];
             $row[] = $request['start'] + ($index + 1);
-            $row[] = '' . $item->ruang;
+            $row[] = ''.$item->ruang;
             $row[] = "<button class='btn btn-warning edit' ><i class='bx bxs-pencil'></i> Edit</button><button class='btn btn-danger delete'><i class='bx bxs-trash-alt' ></i> Hapus</button>";
             $row[] = $item->koderuang;
             $dataFiltered[] = $row;
